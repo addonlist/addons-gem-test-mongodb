b614b9245ea45e2d36c0f19ce81148a1e69ebebc
Ben Pellow <benjamin.pellow@gmail.com>
2014-02-06 22:43:48 -0800

    rails new launchbox-gem-test-mongodb -O :snail:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaunchboxGemTestMongodb::Application.config.secret_key_base = '9e84fd3ed536dcdd351cfdf862cbe4d903e8d43dd6b9b399548e19de0244940356f2ea67fbae9779d92c982ddb7edcb0c0dadda0d8db63ebb56cae9d38c76152'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaunchboxGemTestMongodb::Application.config.session_store :cookie_store, key: '_launchbox-gem-test-mongodb_session'
